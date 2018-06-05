7a2deeaf9e148a7c129bd03965510c5391beb5c5
Nik <nliolios24@gmail.com>
2018-06-05 13:28:32 -0700

    fix release script?

--- bin/release_test.sh
+++ bin/release_test.sh
@@ -1,5 +1,5 @@
-python -m pip install --user --upgrade setuptools wheel
+pip install --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
 
-python -m pip install --user --upgrade twine
+pip install twine
 twine upload --repository-url https://test.pypi.org/legacy/ dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD
